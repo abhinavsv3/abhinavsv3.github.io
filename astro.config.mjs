976d7647298f52c2b4f1615c7eeda1f3a64afe6b
Abhinav S V <abhinavsv3@gmail.com>
2026-03-29 19:38:32 -0700

    Updating for the / redriects

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://scalingthoughts.com',
+  trailingSlash: 'always',
   integrations: [tailwind(), mdx(), sitemap()],
   markdown: {
     shikiConfig: {
